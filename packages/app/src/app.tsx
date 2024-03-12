a842b4cd5f59e3d23fc501e531afa43bc7201ba1
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-12 16:09:56 -0700

    add init

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,3 +1,26 @@
+import { useEffect, useRef } from 'react'
+import invariant from 'tiny-invariant'
+import styles from './app.module.scss'
+
 export function App() {
-  return <>TODO</>
+  const ref = useRef<SVGSVGElement>(null)
+
+  useEffect(() => {
+    invariant(ref.current)
+    init(ref.current)
+  }, [])
+
+  return (
+    <svg
+      ref={ref}
+      className={styles.app}
+      viewBox="0 0 100 100"
+    >
+      <circle cx="50" cy="50" r="10" fill="blue"></circle>
+    </svg>
+  )
+}
+
+function init(svg: SVGSVGElement): void {
+  console.log(svg)
 }
