4020a227e7144aa1e696eaa8687282e80babae0f
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-17 12:02:25 -0700

    remove translate

--- packages/app/src/world.ts
+++ packages/app/src/world.ts
@@ -4,16 +4,13 @@ import { Vec2 } from './vec2.js'
 export function initWorld(): World {
   const cells: World['cells'] = {}
 
-  const translate = new Vec2(-2, -4)
-  const { x: tx, y: ty } = translate
-
   const center = new Vec2(0, 0)
   for (let x = -20; x < 20; x++) {
     for (let y = -20; y < 20; y++) {
       const position = new Vec2(x, y)
       const dist = Math.floor(center.sub(position).len())
       if (dist === 7) {
-        const id = `${x + tx}.${y + ty}`
+        const id = `${x}.${y}`
         const hue = 0
         const saturation = 0
         const lightness = 20 + Math.random() * 20
@@ -23,7 +20,7 @@ export function initWorld(): World {
           color,
         }
       } else if (dist < 7) {
-        const id = `${x + tx}.${y + ty}`
+        const id = `${x}.${y}`
         const hue = 120
         const saturation = 40 + Math.random() * 10
         const lightness = 20 + Math.random() * 10
