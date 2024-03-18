5a622085ae2057f1335fee6bc49ae3eb4e47610f
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-17 17:29:33 -0700

    refactor cell type

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -66,6 +66,19 @@ function usePath(
     let traveled = 0
 
     while (traveled !== total) {
+      const cell = new Vec2(
+        stepX < 0 && x % 1 === 0 ? x - 1 : Math.floor(x),
+        stepY < 0 && y % 1 === 0 ? y - 1 : Math.floor(y),
+      )
+      invariant(cell.x % 1 === 0)
+      invariant(cell.y % 1 === 0)
+      const cellId = `${cell.x}.${cell.y}`
+      const cellType = world.cells[cellId]?.type
+
+      if (cellType !== CellType.enum.Grass) {
+        break
+      }
+
       const tMaxX =
         dir.x === 0
           ? Number.POSITIVE_INFINITY
@@ -94,44 +107,6 @@ function usePath(
 
       invariant(dist >= 0)
       const v = dir.mul(dist)
-      const cell = new Vec2(
-        stepX < 0 && x % 1 === 0 ? x - 1 : Math.floor(x),
-        stepY < 0 && y % 1 === 0 ? y - 1 : Math.floor(y),
-      )
-      invariant(cell.x % 1 === 0)
-      invariant(cell.y % 1 === 0)
-
-      const cellId = `${cell.x}.${cell.y}`
-      const cellType = world.cells[cellId]?.type
-
-      if (cellType !== CellType.enum.Grass) {
-        // let vModified: Vec2 | null = null
-
-        // if (Math.abs(v.x) > Math.abs(v.y)) {
-
-        //   // check if we can still move in the x direction
-
-        //   if (x % 1 === 0) {
-
-        //     const adjacent = new Vec2(
-        //       cell.x,
-        //       cell.y - stepY
-        //     )
-        //     const adjacentId = `${adjacent.x}.${adjacent.y}`
-        //     const adjacentType = world.cells[adjacentId]?.type
-
-        //     if (adjacentType === CellType.enum.Grass) {
-        //       // cannot move, both cells in the x direction are blocked
-        //       vModified = new Vec2(v.x, 0)
-        //     }
-
-        //   } else {
-        //     vModified = new Vec2(v.x)
-        //   }
-        // }
-
-        break
-      }
 
       path.push({ u, v, cell })
 
