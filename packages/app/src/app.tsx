5ad054c3b1e6cc4a9490183541c1e96b8777c90a
Tyler Schloesser <tylerschloesser@gmail.com>
2024-03-23 20:10:22 +0700

    refactor velocity

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -38,17 +38,20 @@ function useVelocity(
   scale: number | null,
   drag: Drag | null,
 ): Vec2 {
-  return useMemo<Vec2>(() => {
+  const fromDrag = useMemo<Vec2 | null>(() => {
     if (scale === null) {
-      return new Vec2(0, 0)
+      return null
     }
     const start = drag?.events.at(0)?.position
     let end = drag?.events.at(-1)?.position
     if (end && start && end.equals(start)) {
       end = undefined
     }
-    const dir =
-      start && end ? end.sub(start) : new Vec2(0, 0)
+    const dir = start && end ? end.sub(start) : null
+
+    if (dir === null) {
+      return null
+    }
 
     // invert y direction
     dir.y *= -1
@@ -60,6 +63,19 @@ function useVelocity(
 
     return dir.norm().mul(speed)
   }, [drag, scale])
+
+  useEffect(() => {
+    let handle: number
+    function step() {
+      handle = self.requestAnimationFrame(step)
+    }
+    handle = self.requestAnimationFrame(step)
+    return () => {
+      self.cancelAnimationFrame(handle)
+    }
+  }, [fromDrag])
+
+  return fromDrag ?? new Vec2(0, 0)
 }
 
 function usePath(
